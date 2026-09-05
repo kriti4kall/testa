65bf88c498977de714876915e573ce22095070c4
kriti4kall <ytiskovich@bk.ru>
2026-09-05 09:18:05 +0300

    Added student info and Pi calculation.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return '<h1>Привет, мир!</h1>'
-app.run(port=3000)
+from http.server import BaseHTTPRequestHandler, HTTPServer
+def pi_gen(digits):
+    base = 10 ** (digits + 10)
+
+    def arc(x):
+        total, p, k = 0, base // x, 1
+        while p > 0:
+            total += (p // k) * (1 if (k // 2) % 2 == 0 else -1)
+            p, k = p // (x * x), k + 2
+        return total
+
+    res = str(4 * (4 * arc(5) - arc(239)) // 10**10)
+    return f"3.{res[1:digits + 1]}"
+class YanServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+
+        fio = "Тискович Ян Юрьевич"
+        group = "477"
+        num = 24
+        pi = pi_gen(num)
+        self.wfile.write(f"{fio}<br>{group}<br>Пи: {pi}".encode("utf-8"))
+bot_server = HTTPServer(("localhost", 3000), YanServer)
+print("Запустился")
+bot_server.serve_forever()
